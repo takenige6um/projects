f90bc24134337f16a7c003cbb582a41e1fe03663
Yoshio Inomoto <oreigen0441@icloud.com>
2019-01-11 20:34:37 +0900

    Fix error

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -628,8 +628,10 @@
 
 (global-set-key (kbd "C-M-t") 'google-translate-enja-or-jaen)
 
-
-
+;; Fix error of "Failed to search TKK"
+(defun google-translate--get-b-d1 ()
+    ;; TKK='427110.1469889687'
+  (list 427110 1469889687))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; 5.6 表示・装飾に関する設定                                 ;;
