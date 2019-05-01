8f5f2e00d5964d6e12d7ea7ce6b2afddacd893a5
Yoshio Inomoto <oreigen0441@icloud.com>
2019-05-01 18:55:10 +0900

    add files

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -125,6 +125,10 @@
 
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 行、リージョン関係                                        ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; 行コピー、行キル
 (defun copy-whole-line (&optional arg)
   "Copy current line."
@@ -183,6 +187,65 @@
   (require 'duplicate-thing)
   (global-set-key (kbd "M-c") 'duplicate-thing))
 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; easy-kill                                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; 詳しい機能は、 M-w の後に、 ? を押して表示されるヘルプを確認してほしいのですが、ここでは覚えておくと便利な汎用的なショートカットを紹介します。
+
+;; 複数行をまとめてコピー
+;; M-w に続けて、 + や - を押すと選択範囲の行が増減して、そのままコピーされます。
+
+;; 現在のバッファのファイルパスをコピー
+;; M-w b を押すと、現在のバッファのフルパス名がコピーされます。
+
+;; 追記コピー
+;; 通常はコピーする毎に、新しい内容で上書きされますが、 M-w @ を押していくと、現在のコピー内容の末尾に追記でコピーしていってくれます。
+
+;; ショートカット M-w に easy-kill をバインド
+(global-set-key [remap kill-ring-save] 'easy-kill)
+
+
+
+;; regionの上下移動
+
+(defun move-text-internal (arg)
+   (cond
+    ((and mark-active transient-mark-mode)
+     (if (> (point) (mark))
+            (exchange-point-and-mark))
+     (let ((column (current-column))
+              (text (delete-and-extract-region (point) (mark))))
+       (forward-line arg)
+       (move-to-column column t)
+       (set-mark (point))
+       (insert text)
+       (exchange-point-and-mark)
+       (setq deactivate-mark nil)))
+    (t
+     (beginning-of-line)
+     (when (or (> arg 0) (not (bobp)))
+       (forward-line)
+       (when (or (< arg 0) (not (eobp)))
+            (transpose-lines arg))
+       (forward-line -1)))))
+
+(defun move-text-down (arg)
+   "Move region (transient-mark-mode active) or current line
+  arg lines down."
+   (interactive "*p")
+   (move-text-internal arg))
+
+(defun move-text-up (arg)
+   "Move region (transient-mark-mode active) or current line
+  arg lines up."
+   (interactive "*p")
+   (move-text-internal (- arg)))
+
+(global-set-key [\M-\S-up] 'move-text-up)
+(global-set-key [\M-\S-down] 'move-text-down)
 
 
 
@@ -653,11 +716,13 @@
 (global-set-key (kbd "C-M-t") 'google-translate-enja-or-jaen)
 
 ;; Fix error of "Failed to search TKK"
-(defun google-translate--get-b-d1 ()
+;; (defun google-translate--get-b-d1 ()
     ;; TKK='427110.1469889687'
  ;; (list 427110 1469889687))
   ;; now we have this representation: `TKK='427116.3269864724';
-  (list 427116 3269864724))
+  ;;(list 427116 3269864724))
+(defun google-translate--search-tkk () "Search TKK." (list 430675 2721866130))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; 5.6 表示・装飾に関する設定                                 ;;
